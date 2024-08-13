781631aa672e40245ce75a8e795869a2607f7dc4
Lukas Klipp <klipp.lukas@gmail.com>
2024-08-13 12:46:26 +0200

    final

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,24 @@
 import GlobalStyle from "../styles";
+import useSWR from "swr";
+import Footer from "@/components/Footer/Footer";
+
+const URL = "https://example-apis.vercel.app/api/art";
 
 export default function App({ Component, pageProps }) {
+  const fetcher = (...args) =>
+    fetch(...args).then((response) => response.json());
+
+  const { data, error, isLoading } = useSWR(URL, fetcher);
+
+  if (error) return <div>failed to load</div>;
+  if (isLoading) return <div>loading...</div>;
+  if (!data) return <div>no data</div>;
+
   return (
     <>
       <GlobalStyle />
-      <Component {...pageProps} />
+      <Component {...pageProps} data={data} />
+      <Footer />
     </>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,6 @@
-import useSWR from "swr";
 import { ArtPiece } from "@/components/Art Piece/ArtPiece";
 
-const URL = "https://example-apis.vercel.app/api/art";
-
-export default function HomePage() {
-  const fetcher = (...args) =>
-    fetch(...args).then((response) => response.json());
-
-  const { data, error, isLoading } = useSWR(URL, fetcher);
-
-  if (error) return <div>failed to load</div>;
-  if (isLoading) return <div>loading...</div>;
-  if (!data) return <div>no data</div>;
-
+export default function HomePage({ data }) {
   console.log(data);
 
   return (
